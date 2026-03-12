e62bda88a7dc2380c93ad7f8a26527a7e7e89bac
Baargavan A <145212407+Baargavan-Anandhakumar@users.noreply.github.com>
2026-03-12 10:28:59 +0530

    Time: 0 ms (100%), Space: 46.7 MB (42.48%) - LeetHub

---
+++ 0337-house-robber-iii/0337-house-robber-iii.java
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int rob(TreeNode root) {
+        int res[]=hepl(root);
+        return Math.max(res[0],res[1]);
+    }
+    public int [] hepl(TreeNode root)
+    {
+        if(root==null)
+        return new int[2];
+
+        int left[]=hepl(root.left);
+        int right[]=hepl(root.right);
+
+        int res[]=new int[2];
+
+        res[0]=root.val+left[1]+right[1];
+        res[1]=Math.max(left[0],left[1])+Math.max(right[0],right[1]);
+        return res;
+    }
+}
