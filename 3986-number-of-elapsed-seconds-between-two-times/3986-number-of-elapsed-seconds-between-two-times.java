53c9c069ccca45b152db9d2468be51a269983733
Baargavan A <145212407+Baargavan-Anandhakumar@users.noreply.github.com>
2026-07-14 15:08:47 +0530

    Time: 3 ms (54.44%), Space: 44.7 MB (31.01%) - LeetHub

--- 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.java
+++ 3986-number-of-elapsed-seconds-between-two-times/3986-number-of-elapsed-seconds-between-two-times.java
@@ -1,14 +1,16 @@
 class Solution {
     public int secondsBetweenTimes(String startTime, String endTime) {
-        String start[] = startTime.split(":");
-        String end[] = endTime.split(":");
-        int st = 0,e = 0;
-        int time = 3600;
-        for(int i=0;i<3;i++){
-            st += Integer.parseInt(start[i])*time;
-            e += Integer.parseInt(end[i])*time;
-            time /= 60;
+        String st[]=startTime.split(":");
+        String en[]=endTime.split(":");
+        int time=3600;
+        int l=0;
+        int f=0;
+        for(int i=0;i<st.length;i++)
+        {
+            l=l+Integer.parseInt(st[i])*time;
+            f=f+Integer.parseInt(en[i])*time;
+            time/=60;
         }
-        return e-st;
+        return f-l;
     }
 }
