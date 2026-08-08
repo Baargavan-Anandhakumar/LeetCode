1fea3553872c486ac59d491181fb73dac8875a90
Baargavan A <145212407+Baargavan-Anandhakumar@users.noreply.github.com>
2026-08-08 14:11:42 +0530

    Time: 325 ms (5.07%), Space: 148.6 MB (25.81%) - LeetHub

---
+++ 3295-report-spam-message/3295-report-spam-message.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean reportSpam(String[] message, String[] bannedWords) {
+        Set<String> set = new TreeSet<>();
+        int c = 0;
+        for (int i = 0; i < bannedWords.length; i++)
+            set.add(bannedWords[i]);
+        for (int i = 0; i < message.length; i++) {
+            if (set.contains(message[i]))
+                c++;
+            if (c == 2)
+                return true;
+        }
+        return false;
+    }
+}
